df7239dad95d05d8a757cd8b19d93fa35bf80c5c
koooota <eno6k21@gmail.com>
2016-10-29 09:42:24 +0000

    bug fix

--- iWant/db/schema.rb
+++ iWant/db/schema.rb
@@ -12,14 +12,14 @@
 
 ActiveRecord::Schema.define(version: 20161029080828) do
 
-  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "CATEDORY_ID"
     t.string   "NAME"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
-  create_table "count_wants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "count_wants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "IDEA"
     t.integer  "CATEGORY_ID"
     t.string   "TITLE"
@@ -28,22 +28,13 @@ ActiveRecord::Schema.define(version: 20161029080828) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "list_goods", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "list_goods", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "WANT_ID"
     t.integer  "USER_ID"
     t.string   "NAME"
     t.integer  "COUNT"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "places", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string   "name"
-    t.string   "description"
-    t.float    "latitude",    limit: 24
-    t.float    "longitude",   limit: 24
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
