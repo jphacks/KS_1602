308b6ea7c2f9cf8a09c9d98c6c23284f4cf3dc9b
koooota <eno6k21@gmail.com>
2016-10-29 04:50:16 +0000

    wants-migration

--- iWant/db/schema.rb
+++ iWant/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028141429) do
+ActiveRecord::Schema.define(version: 20161029025743) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,18 @@ ActiveRecord::Schema.define(version: 20161028141429) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "wants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "USER_ID"
+    t.string   "TITLE"
+    t.integer  "CATEGORY_ID"
+    t.text     "COMMENT",     limit: 65535
+    t.float    "WEFL",        limit: 24
+    t.float    "MERIDIAN",    limit: 24
+    t.integer  "GOOD"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
