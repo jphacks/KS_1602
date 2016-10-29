b9a940015117ed141eb4fc70b999182cb897433f
kazuki <kazuki0563@yahoo.co.jp>
2016-10-29 06:17:57 +0000

    categories migrate

--- iWant/db/schema.rb
+++ iWant/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161029025743) do
+ActiveRecord::Schema.define(version: 20161029060703) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "CATEDORY_ID"
+    t.string   "NAME"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
