7e103795b710be7fef7ea66b1687afd3601b9944
koooota <eno6k21@gmail.com>
2016-11-03 19:50:27 +0900

    add db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,14 @@
 
 ActiveRecord::Schema.define(version: 20161029080828) do
 
-  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "CATEDORY_ID"
     t.string   "NAME"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
-  create_table "count_wants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "count_wants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "IDEA"
     t.integer  "CATEGORY_ID"
     t.string   "TITLE"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20161029080828) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "list_goods", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "list_goods", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "WANT_ID"
     t.integer  "USER_ID"
     t.string   "NAME"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20161029080828) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20161029080828) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  create_table "wants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "wants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "USER_ID"
     t.string   "TITLE"
     t.integer  "CATEGORY_ID"
